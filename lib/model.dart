acf5ca124870aa6fb25575308e1beaa5b46045f6
VinayakaKS <vinayakaksgupta@gmail.com>
2022-05-22 17:10:47 +0530

    added a model class for flask integration

---
+++ lib/model.dart
@@ -0,0 +1,19 @@
+class data {
+  final List<String> toxic;
+  final String recommendation;
+
+  const data({required this.toxic, required this.recommendation});
+
+  factory data.fromJson(Map<String, dynamic> json) {
+    return data(
+        recommendation: json["Recommendation"],
+        toxic: json['Toxic'].cast<String>());
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['Toxic'] = toxic;
+    data['Recommendation'] = recommendation;
+    return data;
+  }
+}
